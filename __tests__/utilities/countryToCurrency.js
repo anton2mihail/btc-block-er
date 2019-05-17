22c731c0f98cefe0106c1a3f7e0066c1926656ae
toni <antonmihail@gmail.com>
2019-05-16 23:45:36 -0400

    Set up test env

---
+++ __tests__/utilities/countryToCurrency.js
@@ -0,0 +1,9 @@
+import { shallow } from 'enzyme';
+
+import coutryToCurrency from '../../utilities/countryToCurrency';
+
+describe('country to currency', () => {
+  it('exists', () => {
+    expect(coutryToCurrency).toBeDefined();
+  });
+});
